9d04d09f3f37fc021bad59f8f4b9c0c5e7f0eca0
Rh4096 <rhelvetican@gmail.com>
2024-03-27 01:32:48 +0700

    switch

--- src/lib.rs
+++ src/lib.rs
@@ -6,24 +6,24 @@ use std::{
 };
 
 #[derive(Debug, Clone, PartialEq, Default)]
-pub(crate) struct Wrapper(String);
+pub(crate) struct Str(String);
 
 #[allow(dead_code)]
-impl Wrapper {
+impl Str {
     pub fn new() -> Self {
-        Wrapper("".to_string())
+        Str("".to_string())
     }
     pub fn from(s: &str) -> Self {
-        Wrapper(s.to_string())
+        Str(s.to_string())
     }
     pub fn from_string(s: String) -> Self {
-        Wrapper(s)
+        Str(s)
     }
     pub fn to_str(&self) -> &str {
         &self.0
     }
     pub fn with_capacity(capacity: usize) -> Self {
-        Wrapper(String::with_capacity(capacity))
+        Str(String::with_capacity(capacity))
     }
     pub fn capacity(&self) -> usize {
         self.0.capacity()
@@ -45,82 +45,74 @@ impl Wrapper {
     }
 }
 
-impl From<&str> for Wrapper {
+impl From<&str> for Str {
     fn from(s: &str) -> Self {
-        Wrapper(s.to_string())
+        Str(s.to_string())
     }
 }
 
-impl From<String> for Wrapper {
+impl From<String> for Str {
     fn from(s: String) -> Self {
-        Wrapper(s)
+        Str(s)
     }
 }
-// unsafe impl Send for Wrapper {
-//     // This is safe because Wrapper is not a reference type
-// }
 
-// unsafe impl Sync for Wrapper {
-//     // This is safe because Wrapper is not a reference type
-// }
-
-impl Display for Wrapper {
+impl Display for Str {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         write!(f, "{}", self.0)
     }
 }
 
-impl Add for Wrapper {
-    type Output = Wrapper;
+impl Add for Str {
+    type Output = Str;
     fn add(self, other: Self) -> Self::Output {
-        Wrapper(self.0 + &other.0)
+        Str(self.0 + &other.0)
     }
 }
 
-impl AddAssign for Wrapper {
+impl AddAssign for Str {
     fn add_assign(&mut self, other: Self) {
         self.0 += &other.0;
     }
 }
 
-impl Sub for Wrapper {
-    type Output = Wrapper;
+impl Sub for Str {
+    type Output = Str;
     fn sub(self, other: Self) -> Self::Output {
-        Wrapper(self.0.replacen(&other.0, "", 1))
+        Str(self.0.replacen(&other.0, "", 1))
     }
 }
 
-impl SubAssign for Wrapper {
+impl SubAssign for Str {
     fn sub_assign(&mut self, other: Self) {
         self.0 = self.0.replacen(&other.0, "", 1)
     }
 }
 
-impl Mul for Wrapper {
-    type Output = Wrapper;
+impl Mul for Str {
+    type Output = Str;
     fn mul(self, other: Self) -> Self::Output {
-        Wrapper(self.0.repeat(other.0.len()))
+        Str(self.0.repeat(other.0.len()))
     }
 }
 
-impl Mul<isize> for Wrapper {
-    type Output = Wrapper;
+impl Mul<isize> for Str {
+    type Output = Str;
     fn mul(self, other: isize) -> Self::Output {
         if other < 0 {
-            Wrapper(
-                self.0
-                    .chars()
-                    .rev()
-                    .collect::<String>()
-                    .repeat(convert_to_usize(other)),
-            )
+            Str(self
+                .0
+                .chars()
+                .rev()
+                .collect::<String>()
+                .repeat(convert_to_usize(other)))
         } else {
-            Wrapper(self.0.repeat(convert_to_usize(other)))
+            Str(self.0.repeat(convert_to_usize(other)))
         }
     }
 }
 
-impl MulAssign<isize> for Wrapper {
+impl MulAssign<isize> for Str {
     fn mul_assign(&mut self, other: isize) {
         if other < 0 {
             self.0 = self
@@ -135,41 +127,39 @@ impl MulAssign<isize> for Wrapper {
     }
 }
 
-impl Mul<Wrapper> for isize {
-    type Output = Wrapper;
-    fn mul(self, other: Wrapper) -> Self::Output {
+impl Mul<Str> for isize {
+    type Output = Str;
+    fn mul(self, other: Str) -> Self::Output {
         if self < 0 {
-            Wrapper(
-                other
-                    .0
-                    .chars()
-                    .rev()
-                    .collect::<String>()
-                    .repeat(convert_to_usize(self)),
-            )
+            Str(other
+                .0
+                .chars()
+                .rev()
+                .collect::<String>()
+                .repeat(convert_to_usize(self)))
         } else {
-            Wrapper(other.0.repeat(convert_to_usize(self)))
+            Str(other.0.repeat(convert_to_usize(self)))
         }
     }
 }
 
-impl MulAssign for Wrapper {
+impl MulAssign for Str {
     fn mul_assign(&mut self, other: Self) {
         self.0 = self.0.repeat(other.0.len());
     }
 }
 
-impl Div<char> for Wrapper {
-    type Output = Wrapper;
+impl Div<char> for Str {
+    type Output = Str;
     fn div(self, other: char) -> Self::Output {
-        Wrapper(match self.0.find(other) {
+        Str(match self.0.find(other) {
             Some(i) => self.0[..i].to_string(),
             None => self.0.clone(),
         })
     }
 }
 
-impl DivAssign<char> for Wrapper {
+impl DivAssign<char> for Str {
     fn div_assign(&mut self, other: char) {
         match self.0.find(other) {
             Some(i) => self.0 = self.0[..i].to_string(),
@@ -178,17 +168,17 @@ impl DivAssign<char> for Wrapper {
     }
 }
 
-impl Rem<char> for Wrapper {
-    type Output = Wrapper;
+impl Rem<char> for Str {
+    type Output = Str;
     fn rem(self, other: char) -> Self::Output {
-        Wrapper(match self.0.rfind(other) {
+        Str(match self.0.rfind(other) {
             Some(i) => self.0[bump(i)..].to_string(),
             None => self.0.clone(),
         })
     }
 }
 
-impl RemAssign<char> for Wrapper {
+impl RemAssign<char> for Str {
     fn rem_assign(&mut self, other: char) {
         match self.0.rfind(other) {
             Some(i) => self.0 = self.0[bump(i)..].to_string(),
@@ -197,24 +187,24 @@ impl RemAssign<char> for Wrapper {
     }
 }
 
-impl Neg for Wrapper {
-    type Output = Wrapper;
+impl Neg for Str {
+    type Output = Str;
     fn neg(self) -> Self::Output {
-        Wrapper(self.0.chars().rev().collect())
+        Str(self.0.chars().rev().collect())
     }
 }
 
-impl Div<&str> for Wrapper {
-    type Output = Wrapper;
+impl Div<&str> for Str {
+    type Output = Str;
     fn div(self, other: &str) -> Self::Output {
-        Wrapper(match self.0.find(other) {
+        Str(match self.0.find(other) {
             Some(i) => self.0[..i].to_string(),
             None => self.0.clone(),
         })
     }
 }
 
-impl DivAssign<&str> for Wrapper {
+impl DivAssign<&str> for Str {
     fn div_assign(&mut self, other: &str) {
         match self.0.find(other) {
             Some(i) => self.0 = self.0[..i].to_string(),
@@ -223,17 +213,17 @@ impl DivAssign<&str> for Wrapper {
     }
 }
 
-impl Rem<&str> for Wrapper {
-    type Output = Wrapper;
+impl Rem<&str> for Str {
+    type Output = Str;
     fn rem(self, other: &str) -> Self::Output {
-        Wrapper(match self.0.rfind(other) {
+        Str(match self.0.rfind(other) {
             Some(i) => self.0[bump(i)..].to_string(),
             None => self.0.clone(),
         })
     }
 }
 
-impl RemAssign<&str> for Wrapper {
+impl RemAssign<&str> for Str {
     fn rem_assign(&mut self, other: &str) {
         match self.0.rfind(other) {
             Some(i) => self.0 = self.0[bump(i)..].to_string(),
@@ -242,17 +232,17 @@ impl RemAssign<&str> for Wrapper {
     }
 }
 
-impl Rem<String> for Wrapper {
-    type Output = Wrapper;
+impl Rem<String> for Str {
+    type Output = Str;
     fn rem(self, other: String) -> Self::Output {
-        Wrapper(match self.0.rfind(other.as_str()) {
+        Str(match self.0.rfind(other.as_str()) {
             Some(i) => self.0[bump(i)..].to_string(),
             None => self.0.clone(),
         })
     }
 }
 
-impl RemAssign<String> for Wrapper {
+impl RemAssign<String> for Str {
     fn rem_assign(&mut self, other: String) {
         match self.0.rfind(other.as_str()) {
             Some(i) => self.0 = self.0[bump(i)..].to_string(),
@@ -268,174 +258,174 @@ fn convert_to_usize(a: isize) -> usize {
 }
 
 fn bump<T>(a: T) -> T
-    where
-        T: Add<Output=T> + From<u8>,
+where
+    T: Add<Output = T> + From<u8>,
 {
     a + T::from(1)
 }
 
 #[cfg(test)]
 mod test {
-    use crate::Wrapper;
+    use crate::Str;
 
     #[test]
-    fn create_wrapper() {
-        let w = Wrapper::new();
+    fn create_str() {
+        let w = Str::new();
         assert_eq!(w.0, "".to_string());
     }
 
     #[test]
-    fn create_wrapper_from_string() {
-        let w = Wrapper::from("hello");
+    fn create_str_from_string() {
+        let w = Str::from("hello");
         assert_eq!(w.0, "hello".to_string());
     }
 
     #[test]
-    fn add_wrapper() {
-        let w1 = Wrapper::from("hello ");
-        let w2 = Wrapper::from("world");
+    fn add_str() {
+        let w1 = Str::from("hello ");
+        let w2 = Str::from("world");
         let w3 = w1 + w2;
         assert_eq!(w3.0, "hello world".to_string());
     }
 
     #[test]
-    fn add_assign_wrapper() {
-        let mut w1 = Wrapper::from("hello ");
-        let w2 = Wrapper::from("world");
+    fn add_assign_str() {
+        let mut w1 = Str::from("hello ");
+        let w2 = Str::from("world");
         w1 += w2;
         assert_eq!(w1.0, "hello world".to_string());
     }
 
     #[test]
-    fn sub_wrapper() {
-        let w1 = Wrapper::from("hello world");
-        let w2 = Wrapper::from(" world");
+    fn sub_str() {
+        let w1 = Str::from("hello world");
+        let w2 = Str::from(" world");
         let w3 = w1 - w2;
         assert_eq!(w3.0, "hello".to_string());
     }
 
     #[test]
-    fn sub_assign_wrapper() {
-        let mut w1 = Wrapper::from("hello world");
-        let w2 = Wrapper::from(" world");
+    fn sub_assign_str() {
+        let mut w1 = Str::from("hello world");
+        let w2 = Str::from(" world");
         w1 -= w2;
         assert_eq!(w1.0, "hello".to_string());
     }
 
     #[test]
-    fn mul_wrapper() {
-        let w1 = Wrapper::from("hello ");
-        let w2 = Wrapper::from("world");
-        let w3: Wrapper = w1 * w2; // Add type annotation to specify the output type
+    fn mul_str() {
+        let w1 = Str::from("hello ");
+        let w2 = Str::from("world");
+        let w3: Str = w1 * w2; // Add type annotation to specify the output type
         assert_eq!(w3.0, "hello hello hello hello hello ".to_string());
     }
 
     #[test]
-    fn mul_wrapper_with_int() {
-        let w1 = Wrapper::from("hello ");
+    fn mul_str_with_int() {
+        let w1 = Str::from("hello ");
         let w2: isize = 3;
-        let w3: Wrapper = w1 * Into::<isize>::into(w2);
+        let w3: Str = w1 * Into::<isize>::into(w2);
         assert_eq!(w3.0, "hello hello hello ".to_string());
     }
 
     #[test]
-    fn mul_wrapper_with_neg_int() {
-        let w1 = Wrapper::from("hello ");
+    fn mul_str_with_neg_int() {
+        let w1 = Str::from("hello ");
         let w2: isize = -5;
-        let w3: Wrapper = w1 * w2;
+        let w3: Str = w1 * w2;
         assert_eq!(w3.0, " olleh olleh olleh olleh olleh".to_string());
     }
 
     #[test]
-    fn mul_assign_wrapper() {
-        let mut w1 = Wrapper::from("hello ");
-        let w2 = Wrapper::from("world");
+    fn mul_assign_str() {
+        let mut w1 = Str::from("hello ");
+        let w2 = Str::from("world");
         w1 *= w2;
         assert_eq!(w1.0, "hello hello hello hello hello ".to_string());
     }
 
     #[test]
-    fn mul_assign_wrapper_with_int() {
-        let mut w1 = Wrapper::from("hello ");
+    fn mul_assign_str_with_int() {
+        let mut w1 = Str::from("hello ");
         let w2: isize = 3;
         w1 *= w2;
         assert_eq!(w1.0, "hello hello hello ".to_string());
     }
 
     #[test]
-    fn mul_assign_wrapper_with_neg_int() {
-        let mut w1 = Wrapper::from("hello ");
+    fn mul_assign_str_with_neg_int() {
+        let mut w1 = Str::from("hello ");
         let w2: isize = -5;
         w1 *= w2;
         assert_eq!(w1.0, " olleh olleh olleh olleh olleh".to_string());
     }
 
     #[test]
-    fn div_wrapper() {
-        let w1 = Wrapper::from("hello world");
+    fn div_str() {
+        let w1 = Str::from("hello world");
         let w2: char = ' ';
         let w3 = w1 / w2;
         assert_eq!(w3.0, "hello".to_string());
     }
 
     #[test]
-    fn div_assign_wrapper() {
-        let mut w1 = Wrapper::from("hello world");
+    fn div_assign_str() {
+        let mut w1 = Str::from("hello world");
         let w2: char = ' ';
         w1 /= w2;
         assert_eq!(w1.0, "hello".to_string());
     }
 
     #[test]
-    fn rem_wrapper() {
-        let w1 = Wrapper::from("hello world");
+    fn rem_str() {
+        let w1 = Str::from("hello world");
         let w2: char = ' ';
         let w3 = w1 % w2;
         assert_eq!(w3.0, "world".to_string());
     }
 
     #[test]
-    fn rem_assign_wrapper() {
-        let mut w1 = Wrapper::from("hello world");
+    fn rem_assign_str() {
+        let mut w1 = Str::from("hello world");
         let w2: char = ' ';
         w1 %= w2;
         assert_eq!(w1.0, "world".to_string());
     }
 
     #[test]
-    fn neg_wrapper() {
-        let w1 = Wrapper::from("hello world");
+    fn neg_str() {
+        let w1 = Str::from("hello world");
         let w2 = -w1;
         assert_eq!(w2.0, "dlrow olleh".to_string());
     }
 
     #[test]
-    fn div_wrapper_with_str() {
-        let w1 = Wrapper::from("hello world");
+    fn div_str_with_str() {
+        let w1 = Str::from("hello world");
         let w2 = " ";
         let w3 = w1 / w2;
         assert_eq!(w3.0, "hello".to_string());
     }
 
     #[test]
-    fn div_assign_wrapper_with_str() {
-        let mut w1 = Wrapper::from("hello world");
+    fn div_assign_str_with_str() {
+        let mut w1 = Str::from("hello world");
         let w2 = " ";
         w1 /= w2;
         assert_eq!(w1.0, "hello".to_string());
     }
 
     #[test]
-    fn rem_wrapper_with_str() {
-        let w1 = Wrapper::from("hello world");
+    fn rem_str_with_str() {
+        let w1 = Str::from("hello world");
         let w2 = " ";
         let w3 = w1 % w2;
         assert_eq!(w3.0, "world".to_string());
     }
 
     #[test]
-    fn rem_assign_wrapper_with_str() {
-        let mut w1 = Wrapper::from("hello world");
+    fn rem_assign_str_with_str() {
+        let mut w1 = Str::from("hello world");
         let w2 = " ";
         w1 %= w2;
         assert_eq!(w1.0, "world".to_string());
