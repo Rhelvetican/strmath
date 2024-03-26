97feb5035933e883cb8030d09751728410ad52bd
Rh4096 <rhelvetican@gmail.com>
2024-03-27 04:42:45 +0700

    impl

---
+++ src/imp.rs
@@ -0,0 +1,39 @@
+use crate::Str;
+use std::str::Chars;
+
+#[allow(dead_code)]
+impl Str {
+    pub fn new() -> Self {
+        Str("".to_string())
+    }
+    pub fn from(s: &str) -> Self {
+        Str(s.to_string())
+    }
+    pub fn from_string(s: String) -> Self {
+        Str(s)
+    }
+    pub fn to_str(&self) -> &str {
+        &self.0
+    }
+    pub fn with_capacity(capacity: usize) -> Self {
+        Str(String::with_capacity(capacity))
+    }
+    pub fn capacity(&self) -> usize {
+        self.0.capacity()
+    }
+    pub fn reserve(&mut self, additional: usize) {
+        self.0.reserve(additional);
+    }
+    pub fn reserve_exact(&mut self, additional: usize) {
+        self.0.reserve_exact(additional);
+    }
+    pub fn reverse(&mut self) {
+        self.0 = self.0.chars().rev().collect();
+    }
+    pub fn push(&mut self, s: &str) {
+        self.0 += s;
+    }
+    pub fn chars(&self) -> Chars<'_> {
+        self.0.chars()
+    }
+}
