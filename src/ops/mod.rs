f45e067d22013d5164a5d12431a6f874a0a21113
Rh4096 <rhelvetican@gmail.com>
2024-04-01 08:53:31 +0700

    bsd

--- src/ops/mod.rs
+++ src/ops/mod.rs
@@ -1,3 +1,30 @@
+//! # Operations
+//! Available operations for `Str` type are:
+//! * Add (+)
+//! * AddAssign (+=)
+//! * Sub (-)
+//! * SubAssign (-=)
+//! * Mul (*)
+//! * MulAssign (*=)
+//! * Div (/)
+//! * DivAssign (/=)
+//! * Rem (%)
+//! * RemAssign (%=)
+//!
+//! [add]: crate::ops::add
+//! [sub]: crate::ops::sub
+//! [mul]: crate::ops::mul
+//! [div]: crate::ops::div
+//! [rem]: crate::ops::rem
+//!
+//! ## Add
+//! The `Add` trait is used to add a string to another string or anything that implements the trait `Display`.
+//! ```rust
+//! use strmath::Str;
+//! let s = Str::from("Hello, ");
+//! let x = Str::from("World!");
+//!
+
 #[cfg(feature = "add")]
 pub mod add;
 #[cfg(feature = "div")]
